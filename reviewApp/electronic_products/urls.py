f9bc59648a102b9e34ae5150b0b4b2c845bc69b6
sarah19990 <sara1999200407@gmail.com>
2023-04-03 09:36:05 +0000

    add images footer and text  to the about us page

--- reviewApp/electronic_products/urls.py
+++ reviewApp/electronic_products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns =[
     path('', views.home, name='electronic_products-home'),
     path('about/', views.about, name='electronic_products-about'),
     path('contact/', views.contact, name='electronic_products-contact'),
     path('product/', views.product, name='electronic_products-product'),
-]
+] 
