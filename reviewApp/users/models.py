b0c064359cad710393ddb04f77c30482c9d2fb03
sarah19990 <sara1999200407@gmail.com>
2023-04-04 11:46:48 +0000

    adding product to product page and populated with information defined in model

--- reviewApp/users/models.py
+++ reviewApp/users/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=True)
+    brand = models.CharField(max_length=50, null=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    category = models.CharField(max_length=50, null=True)
+    release_of_date = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True,null=True)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
+
+    def __str__(self):
+        return f' The Product is a {self.name} '
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
